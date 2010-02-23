80419a32a4a9e4450ee74afece80acfee0cd9c7e
Eric Normand <ericwnormand@gmail.com>
2010-02-22 19:34:04 -0600

    Initial commit did not work.  now it does

--- src/reasoner.clj
+++ src/reasoner.clj
@@ -18,56 +18,66 @@
 
 (defn build-graph
   [stmts]
-  (reduce add-edge 
-	  (new-graph) 
-	  stmts))
+  (reduce add-edge (new-graph) stmts))
 
 (defn query-single
-  "Apply a restriction to the graph.  All edge elements i must be = to v.  If v
+  "Apply a restriction to the graph.
+All edge elements i must be = to v.  If v
 is nil, no restriction is applied."
-  [g [p i v]]
- (if (nil? v)
-   g
-   (build-graph (filter #(p (% i) v) (:edges g)))))
+  [g [i v]]
+  (if (nil? v)
+    g
+    (build-graph (filter #(= (% i) v) (:edges g)))))
 
 (defn query-edges
- [g & args]
- (reduce #(query-single %1 %2) 
-	 g 
-	 args))
+  [graph & args]
+  (reduce #(query-single %1 %2) graph args))
 
 (defn variable?
- [x]
- (and (symbol? x)
-      (.startsWith (str x) "?")))
+  [x]
+  (and (symbol? x)
+       (.startsWith (str x) "?")))
+
+(defn vars
+  [stmt]
+  (for [[idx var] stmt :when (variable? var)] [idx var]))
+
+(defn non-vars
+  [stmt]
+  (for [[idx val] stmt :when (not (variable? val))] [idx val]))
 
 (defn rehash
- [s]
- (apply hash-map (apply concat s)))
+  [s]
+  (apply hash-map (apply concat s)))
+
+
 
 ;;; a solution is a set of bindings and a graph that matches that binding
 ;;; a statement is an edge with optional parts and variable parts
 (defn gensols
- [g stmt]
- (let [vars (filter #(variable? (% 1)) stmt)
-       non-vars (apply concat (filter #(not (variable? (% 1))) stmt))
-       edges (apply query-edges g non-vars)]
-   (set (for [e (:edges edges)]
-          {:graph (build-graph [e])
-           :bindings (rehash (for [[k v] vars]
-                                [v (e k)]))}))))
+ [graph stmt]
+ (let [	;; vars get bound to values
+       vars (vars stmt)
+       ;; non-vars are values that must match in the graph
+       non-vars (non-vars stmt)
+       ;; 
+       edges (apply query-edges graph non-vars)]
+   (set (for [edge (:edges edges)]
+          {:graph (build-graph [edge])
+           :bindings (rehash (for [[idx var] vars]
+			       [var (edge idx)]))}))))
 
 (defn merge-graphs
   [g1 g2]
   (reduce add-edge g1 (:edges g2)))
 
 (defn combine-sols
- [s1 s2]
- (if (every? identity (for [[k v] (:bindings s1) :when ((:bindings s2) k)]
-                        (= v ((:bindings s2) k))))
-   {:bindings (merge (:bindings s1) (:bindings s2))
-    :graph (merge-graphs (:graph s1)
-			 (:graph s2))}))
+  [s1 s2]
+  (if (every? identity (for [[k v] (:bindings s1) :when ((:bindings s2) k)]
+			 (= v ((:bindings s2) k))))
+    {:bindings (merge (:bindings s1) (:bindings s2))
+     :graph (merge-graphs (:graph s1)
+			  (:graph s2))}))
 
 (defn cross
   [f l1 l2]
@@ -79,10 +89,11 @@ is nil, no restriction is applied."
   (set (filter #(not (nil? %)) (cross combine-sols sols (gensols g stmt)))))
 
 (defn query
- [g stmts]
- (reduce #(q g %1 %2)
+ [graph stmts]
+ (reduce #(q graph %1 %2)
 	 #{{:graph (new-graph) :bindings {}}}
 	 stmts))
+
 
 (def test-graph
      (build-graph [
